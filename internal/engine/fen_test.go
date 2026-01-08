dcabdefeb604052f48c9f71ee80a737cca09a443
Mgrdich <mgotm13@gmail.com>
2026-01-08 20:25:34 +0400

    test: add 20 famous perft positions to FEN round-trip tests

--- internal/engine/fen_test.go
+++ internal/engine/fen_test.go
@@ -1125,6 +1125,87 @@ func TestRoundTripParseFENToFEN(t *testing.T) {
 			name: "position 5",
 			fen:  "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
 		},
+		// Additional famous perft test positions from chessprogramming.org
+		{
+			name: "perft - position with bishops and rooks",
+			fen:  "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2",
+		},
+		{
+			name: "perft - en passant promotion",
+			fen:  "8/8/8/2k5/2pP4/8/B7/4K3 b - d3 0 3",
+		},
+		{
+			name: "perft - discovered check position",
+			fen:  "r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2",
+		},
+		{
+			name: "perft - checkmate in few moves",
+			fen:  "2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2",
+		},
+		{
+			name: "perft - complex promotion scenario",
+			fen:  "rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9",
+		},
+		{
+			name: "perft - endgame pawn race",
+			fen:  "2r5/3pk3/8/2P5/8/2K5/8/8 w - - 5 4",
+		},
+		{
+			name: "perft - rook endgame",
+			fen:  "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1",
+		},
+		{
+			name: "perft - minimal pieces",
+			fen:  "8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1",
+		},
+		{
+			name: "perft - en passant edge case",
+			fen:  "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1",
+		},
+		{
+			name: "perft - kingside castling rights only",
+			fen:  "5k2/8/8/8/8/8/8/4K2R w K - 0 1",
+		},
+		{
+			name: "perft - queenside castling rights only",
+			fen:  "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1",
+		},
+		{
+			name: "perft - bishops and rooks symmetry",
+			fen:  "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1",
+		},
+		{
+			name: "perft - queens and rooks",
+			fen:  "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1",
+		},
+		{
+			name: "perft - promotion to rook",
+			fen:  "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1",
+		},
+		{
+			name: "perft - knight and queen endgame",
+			fen:  "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1",
+		},
+		{
+			name: "perft - pawn promotion on 7th rank",
+			fen:  "4k3/1P6/8/8/8/8/K7/8 w - - 0 1",
+		},
+		{
+			name: "perft - pawn promotion corner",
+			fen:  "8/P1k5/K7/8/8/8/8/8 w - - 0 1",
+		},
+		{
+			name: "perft - pawn promotion king corner",
+			fen:  "K1k5/8/P7/8/8/8/8/8 w - - 0 1",
+		},
+		{
+			name: "perft - black pawn promotion",
+			fen:  "8/k1P5/8/1K6/8/8/8/8 w - - 0 1",
+		},
+		{
+			name: "perft - checkmate pattern",
+			fen:  "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1",
+		},
 	}
 
 	for _, tt := range tests {
